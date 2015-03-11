4eec4063075ca3b9ee8d43aa8c5ed1e6894f78b9
Joshua Christopherson <jechristop1@gmail.com>
2015-03-11 13:56:15 -0700

    started to create generator form

---
+++ js/app.js
@@ -0,0 +1,21 @@
+(function(){
+
+    var upperEx, lowerEx, cardioEx;
+
+    var barbellUpper = ['Bench Press','Military Press','Bent-over Row'];
+    var barbellLower = ['Back Squat','Front Squat','Deadlift'];
+    var dumbellUpper = ['Dumbell Incline Bench','Dumbell Press','Dumbell Curls'];
+    var dumbellLower = ['Dumbell Lunge','Dumbell Stiff-leg Deadlift','Dumbell Step Ups'];
+    var cardio = ['Sled','Treadmill','Bike'];
+
+    function generateUpperExcercise(upperEx){
+        return upperBodyExercise = myArray[Math.floor(Math.random() * upperEx.length)];
+    };
+
+    function generateLowerExcercise(){
+
+    };
+
+
+
+})();
