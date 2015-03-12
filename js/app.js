4378fff6aa750ae1b00b919acbb77332b1a36dd3
Joshua Christopherson <jechristop1@gmail.com>
2015-03-11 23:01:46 -0700

    added some javascript in attempt to create random workouts

--- js/app.js
+++ js/app.js
@@ -1,20 +1,40 @@
 (function(){
 
-    var upperEx, lowerEx, cardioEx;
+    var upperEx, 
+        lowerEx, 
+        cardioEx,
+        i;
 
     var barbellUpper = ['Bench Press','Military Press','Bent-over Row'];
     var barbellLower = ['Back Squat','Front Squat','Deadlift'];
     var dumbellUpper = ['Dumbell Incline Bench','Dumbell Press','Dumbell Curls'];
     var dumbellLower = ['Dumbell Lunge','Dumbell Stiff-leg Deadlift','Dumbell Step Ups'];
     var cardio = ['Sled','Treadmill','Bike'];
+    
+    var upperOutputDiv = document.getElementById("upperOutput");
 
+    upperEx = barbellUpper.concat(dumbellUpper);    
+    
     function generateUpperExcercise(upperEx){
-        return upperBodyExercise = myArray[Math.floor(Math.random() * upperEx.length)];
+        var prevRandExercise = "";
+        var upperBodyExercise;
+        upperOutputDiv.innerHTML = '<ul>';
+        for (i = 1; i <= 4; i++) {
+            prevRandExercise = upperBodyExercise;
+            upperBodyExercise = upperEx[Math.floor(Math.random() * upperEx.length)];
+
+            if (upperBodyExercise === prevRandExercise) {
+                upperBodyExercise = upperEx[Math.floor(Math.random() * upperEx.length)];
+            };   
+            upperOutputDiv.innerHTML += '<li>' + upperBodyExercise + '</li>';
+        };
+        upperOutputDiv.innerHTML += '</ul>';
+
+
     };
 
-    function generateLowerExcercise(){
+    generateUpperExcercise(upperEx);
 
-    };
 
 
 
