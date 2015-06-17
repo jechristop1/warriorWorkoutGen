b450f7d5dca0ba46b02dd72f1297ee53e9d17472
Joshua Christopherson <jechristop1@gmail.com>
2015-06-17 09:39:23 -0700

    more changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-//function generateWorkout(e){
+/*******************
+Arrays and variables
+********************/
+var upperWorkoutMain = [], upperWorkoutAccessory = [], lowerWorkoutMain = [], lowerWorkoutAccessory = [], cardioWorkout = [];
+var totalUpperMain = [1,2,3,4], 
+    totalUpperAccessory = [1,2,3,4], 
+    totalLowerMain = [1,2,3,4], 
+    totalLowerAccessory = [1,2,3,4], 
+    totalCardioEx = [1,2,3,4];
+var barbellUpperDB = ['Bench Press','Military Press','Bent-over Row', 'Push Press', 'Close-grip Bench Press', 'barbell curl', 'blu7', 'blu8', 'blu9', 'blu10'];
+var barbellLowerDB = ['Back Squat','Front Squat','Deadlift', 'Stiff-legged Deadlift', 'Barbell lunge', 'Trap bar Deadlift', 'bll7', 'bll8', 'bll9', 'bll10'];
+var dumbellUpperDB = ['Dumbell Incline Bench','Dumbell Press','Dumbell Curls', 'Shoulder Laterals', 'Tricep Extension', 'Hammer curl', 'dlu7', 'dlu8', 'dlu9', 'dlu10' ];
+var dumbellLowerDB = ['Dumbell Lunge','Dumbell Stiff-leg Deadlift','Dumbell Step Ups', 'Sumo Squat', 'dll5', 'dll6', 'dll7', 'dll8', 'dll8', 'dll9', 'dll10'];
+var cardioDB = ['sprint', 'jumping jacks', 'power walk' , 'jump rope'];
+var kettlebellUpperDB = ['kbu1', 'kbu2','kbu3', 'kbu4','kbu5', 'kbu6','kbu7', 'kbu8', 'kbu9', 'kbu10'];
+var kettlebellLowerDB = ['kbl1', 'kbl2','kbl3', 'kbl4','kbl5', 'kbl6','kbl7', 'kbl8', 'kbl9', 'kbl10'];
+var trxUpperDB = ['TRX Chest Press', 'TRX Ys and Ts', 'TRX Bicep Curl', 'TRX Tricep Press', 'TRX Low Row', 'TRXu 6', 'TRXu 7', 'TRXu 8', 'TRXu 9', 'TRXu 10'];
+var trxLowerDB = ['TRX Squat', 'TRX Lunge', 'TRXl 3', 'TRXl 4', 'TRXl 5', 'TRXl 6', 'TRXl 7', 'TRXl 4', 'TRXl 3', 'TRXl 4'];
 
 /*******************
 Make Button like checkbox
@@ -17,26 +34,6 @@ function checkCheckbox(){
         }
     }
 };
-
-/*******************
-Arrays and variables
-********************/
-
-var upperWorkoutMain = [], upperWorkoutAccessory = [], lowerWorkoutMain = [], lowerWorkoutAccessory = [], cardioWorkout = [];
-var totalUpperMain = [1,2,3,4], 
-    totalUpperAccessory = [1,2,3,4], 
-    totalLowerMain = [1,2,3,4], 
-    totalLowerAccessory = [1,2,3,4], 
-    totalCardioEx = [1,2,3,4];
-var barbellUpperDB = ['Bench Press','Military Press','Bent-over Row', 'Push Press', 'Close-grip Bench Press', 'barbell curl', 'blu7', 'blu8', 'blu9', 'blu10'];
-var barbellLowerDB = ['Back Squat','Front Squat','Deadlift', 'Stiff-legged Deadlift', 'Barbell lunge', 'Trap bar Deadlift', 'bll7', 'bll8', 'bll9', 'bll10'];
-var dumbellUpperDB = ['Dumbell Incline Bench','Dumbell Press','Dumbell Curls', 'Shoulder Laterals', 'Tricep Extension', 'Hammer curl', 'dlu7', 'dlu8', 'dlu9', 'dlu10' ];
-var dumbellLowerDB = ['Dumbell Lunge','Dumbell Stiff-leg Deadlift','Dumbell Step Ups', 'Sumo Squat', 'dll5', 'dll6', 'dll7', 'dll8', 'dll8', 'dll9', 'dll10'];
-var cardioDB = ['sprint', 'jumping jacks', 'power walk' , 'jump rope'];
-var kettlebellUpperDB = ['kbu1', 'kbu2','kbu3', 'kbu4','kbu5', 'kbu6','kbu7', 'kbu8', 'kbu9', 'kbu10'];
-var kettlebellLowerDB = ['kbl1', 'kbl2','kbl3', 'kbl4','kbl5', 'kbl6','kbl7', 'kbl8', 'kbl9', 'kbl10'];
-var trxUpperDB = ['TRX Chest Press', 'TRX Ys and Ts', 'TRX Bicep Curl', 'TRX Tricep Press', 'TRX Low Row', 'TRXu 6', 'TRXu 7', 'TRXu 8', 'TRXu 9', 'TRXu 10'];
-var trxLowerDB = ['TRX Squat', 'TRX Lunge', 'TRXl 3', 'TRXl 4', 'TRXl 5', 'TRXl 6', 'TRXl 7', 'TRXl 4', 'TRXl 3', 'TRXl 4'];
 
 /*************************************************************
 Checkbox and concat here so array is created before page loads
@@ -128,42 +125,61 @@ var exerciseGenerator = function(workoutArray, exerDatabase){
         }
     }
     return workoutArray;
-};
+}; 
+
 
 /*******************
 Put Exercises in HTML
 ********************/
 
-// var upperMain = exerciseGenerator(upperWorkoutMain, totalUpperMain);
-// console.log( upperMain);
 
-// var upperAccessory = exerciseGenerator(upperWorkoutAccessory, totalUpperAccessory);
-// console.log( upperAccessory);
+var workoutDivs = document.getElementsByClassName("workoutDiv");
+    
 
-// var lowerMain = exerciseGenerator(lowerWorkoutMain, totalLowerMain);
-// console.log(lowerMain);
+function makeWorkouts(e){
 
-// var lowerAccessory = exerciseGenerator(lowerWorkoutAccessory, totalLowerAccessory);
-// console.log(lowerAccessory);
-
-// var cardio = exerciseGenerator(cardioWorkout, totalCardioEx);
-// console.log(cardio);
-
-// var workoutDivs = document.getElementsByClassName("workoutDiv");
+    for (var i = 0; i < workoutDivs.length; i++) {
+        workoutDivs[i].innerHTML += '<p>' + "" + '</p>';
+        workoutDivs[i].innerHTML += '<p>' + "" + '</p>';
+        workoutDivs[i].innerHTML += '<p>' + "" + '</p>';
+        workoutDivs[i].innerHTML += '<p>' + "" + '</p>';
+        workoutDivs[i].innerHTML += '<p>' + "" + '</p>';
+    }
 
 
-// for (var i = 0; i < workoutDivs.length; i++) {
+    var upperMain = exerciseGenerator(upperWorkoutMain, totalUpperMain);
+    console.log( upperMain);
 
-//     workoutDivs[i].innerHTML += '<p>' + upperMain[i] + '</p>';
-//     workoutDivs[i].innerHTML += '<p>' + upperAccessory[i] + '</p>';
-//     workoutDivs[i].innerHTML += '<p>' + lowerMain[i] + '</p>';
-//     workoutDivs[i].innerHTML += '<p>' + lowerAccessory[i] + '</p>';
-//     workoutDivs[i].innerHTML += '<p>' + cardio[i] + '</p>';
+    var upperAccessory = exerciseGenerator(upperWorkoutAccessory, totalUpperAccessory);
+    console.log( upperAccessory);
 
-// }
+    var lowerMain = exerciseGenerator(lowerWorkoutMain, totalLowerMain);
+    console.log(lowerMain);
+
+    var lowerAccessory = exerciseGenerator(lowerWorkoutAccessory, totalLowerAccessory);
+    console.log(lowerAccessory);
+
+    var cardio = exerciseGenerator(cardioWorkout, totalCardioEx);
+    console.log(cardio);
 
 
-//};  //End of page function
+    for (var i = 0; i < workoutDivs.length; i++) {
+        workoutDivs[i].innerHTML += '<p>' + upperMain[i] + '</p>';
+        workoutDivs[i].innerHTML += '<p>' + upperAccessory[i] + '</p>';
+        workoutDivs[i].innerHTML += '<p>' + lowerMain[i] + '</p>';
+        workoutDivs[i].innerHTML += '<p>' + lowerAccessory[i] + '</p>';
+        workoutDivs[i].innerHTML += '<p>' + cardio[i] + '</p>';
+    }
+
+
+}
+
+
+
+
+
+
+
 
 
 
